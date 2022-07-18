e10184014cd0cc2197065ee36f95e1d733907eab
PBug <pbug90@protonmail.com>
2022-07-18 17:24:31 +0200

    fix: build now includes tailwind css (#6)

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,7 +1,4 @@
-const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
-
 module.exports = {
-  purge: createGlobPatternsForDependencies(__dirname),
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
